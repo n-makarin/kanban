3442561e229797fbc8c0db6989e787c6881bb1f3
Nikita Makarin <n-makarin@mail.ru>
2019-12-23 19:20:41 +0300

    Create src\api\my-json-server.js

---
+++ src/api/my-json-server.js
@@ -0,0 +1,32 @@
+/**
+ * Doc: https://my-json-server.typicode.com/
+ */
+import axios from 'axios'
+
+const endpoint = 'https://my-json-server.typicode.com/n-makarin/kanban/'
+const myJsonServer = {
+  /**
+   * Get current weather by city name
+   * @param {string} path Query path
+   * @param {string} query Query params
+   * @returns any
+   */
+  async get (path = '', query = '') {
+    let rawData = null
+    const url = `${endpoint}${path}${query}`
+    await axios({
+      method: 'GET',
+      url
+    })
+      .catch((error) => {
+        console.log(`Can't get data by url: ${url}. Error: ${error}`)
+      })
+      .then((result) => {
+        if (!result || !result.data) { return }
+        rawData = result.data
+      })
+    return rawData
+  }
+}
+
+export default myJsonServer
