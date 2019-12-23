6ab45a6256f6e1a5cf9c13c3cd6181dbc98ee101
Nikita Makarin <n-makarin@mail.ru>
2019-12-23 19:39:33 +0300

    Add boardList prop & getBoardList() method src\views\Boards.vue

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -6,10 +6,30 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'home',
   components: {
+  },
+  computed: {
+    ...mapGetters({
+      boardList: 'board/list/data'
+    })
+  },
+  data () {
+    return {
+      dataLoaded: false
+    }
+  },
+  methods: {
+    ...mapActions({
+      getBoardList: 'board/list/get'
+    })
+  },
+  async mounted () {
+    await this.getBoardList()
+    this.dataLoaded = true
   }
 }
 </script>
