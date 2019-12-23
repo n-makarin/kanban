c52d5dc1a53dc3d2620f9b39bdb04a5615740430
Nikita Makarin <n-makarin@mail.ru>
2019-12-23 20:30:14 +0300

    Create src\components\board\list.vue

---
+++ src/components/board/list.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list">
+    <list-item
+      v-for="item in data"
+      :key="item.id"
+      :id="item.id"
+      :title="item.title"
+      :order="item.order"
+    />
+  </div>
+</template>
+
+<script>
+import ListItem from '@/components/board/list/item'
+
+export default {
+  components: {
+    ListItem
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
