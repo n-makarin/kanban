77485de6de71c56fec510ce9be011460634dedf4
Nikita Makarin <n-makarin@mail.ru>
2019-12-23 21:35:12 +0300

    Create src\components\board\list\item\card.vue

---
+++ src/components/board/list/item/card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :style="style" class="card">
+    <div class="card__title">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    id: {
+      type: Number,
+      default: 0
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    order: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    style () {
+      return { order: this.order }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
