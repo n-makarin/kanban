36bc2c312a3ae459ddcf25484048500d483658ba
Nikita Makarin <n-makarin@mail.ru>
2019-12-23 19:12:14 +0300

    Create src\views\Boards.vue

---
+++ src/views/Boards.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="boards">
+    boards
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
