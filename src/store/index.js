1529999da3ff4989762debdecdea00470421b514
Nikita Makarin <n-makarin@mail.ru>
2019-12-23 21:34:21 +0300

    Add card module to src\store\index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import board from '@/store/board'
+import card from '@/store/card'
 
 Vue.use(Vuex)
 
@@ -14,6 +15,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-    board
+    board,
+    card
   }
 })
