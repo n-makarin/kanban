388bead64ac3636857d89dae3e500edc72e203e9
Nikita Makarin <n-makarin@mail.ru>
2019-12-23 20:30:22 +0300

    Create src\components\board\list\item.vue

---
+++ src/components/board/list/item.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :style="style" class="item">
+    <div class="item__title">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      default: 0
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    order: {
+      type: Number,
+      default: 1
+    }
+  },
+  computed: {
+    style () {
+      return { order: this.order }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
