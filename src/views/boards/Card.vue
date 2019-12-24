0416d7fe7bbf36c63fff67d31e778462768613d4
Nikita Makarin <n-makarin@mail.ru>
2019-12-24 11:04:20 +0300

    Add murkup, styles & methods to Card.vue

--- src/views/boards/Card.vue
+++ src/views/boards/Card.vue
@@ -1,5 +1,146 @@
 <template>
   <div class="card">
-    <h1>card</h1>
+    <modal-default
+      :visible="true"
+      @close="redirectToPrevPage"
+      class="card__modal card-modal"
+      border-radius="5px"
+      v-if="dataLoaded"
+    >
+      <div class="card-modal__content">
+        <form class="card-modal__form" action="POST">
+          <textarea
+            name="title"
+            v-model="data.title"
+            class="textarea card-modal__title"
+          />
+          <textarea
+            name="description"
+            v-model="data.description"
+            placeholder="Описание"
+            class="textarea card-modal__description"
+          />
+          <button
+            @submit.prevent="save"
+            class="card-modal__button-save"
+          >
+            Сохранить
+          </button>
+        </form>
+        <div @click="redirectToPrevPage" class="card-modal__button-close">
+          <img src="@/assets/images/icons/plus.svg" alt="close">
+        </div>
+      </div>
+    </modal-default>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import ModalDefault from '@/components/layout/modal/default'
+
+export default {
+  components: {
+    ModalDefault
+  },
+  data () {
+    return {
+      dataLoaded: false
+    }
+  },
+  computed: {
+    ...mapGetters({
+      data: 'card/detail/data'
+    })
+  },
+  methods: {
+    ...mapActions({
+      getData: 'card/detail/get'
+    }),
+    redirectToPrevPage () {
+      this.$router.go(-1)
+    },
+    save () {
+
+    }
+  },
+  async mounted () {
+    await this.getData(this.$route.params.id)
+    this.dataLoaded = true
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@mixin textarea-acrive {
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);
+  background: white;
+}
+
+.card {
+  &-modal {
+    border-radius: 5px;
+    &__content {
+      position: relative;
+      width: 600px;
+      padding: 16px 16px 13px 16px;
+      background: $gray-ligh;
+      border-radius: 5px;
+      .textarea {
+        display: block;
+        width: 100%;
+        border-radius: 2px;
+        resize: none;
+        overflow-y: auto;
+        border: none;
+        background: $gray-ligh;
+        outline: none;
+        &:first-child {
+          &:focus {
+            @include textarea-acrive;
+          }
+        }
+        &:nth-child(2) {
+          @include textarea-acrive;
+        }
+      }
+    }
+    &__title {
+      margin-right: 30px;
+      margin-bottom: 12px;
+      font-weight: bold;
+      font-size: 24px;
+      padding: 5px;
+    }
+    &__description {
+      padding: 12px;
+      min-height: 95px;
+      font-size: 15px;
+      color: $black;
+      margin-bottom: 16px;
+      &:placeholder {
+        color: $gray-middle;
+      }
+    }
+    &__button {
+      &-save {
+        background: $blue;
+        border-radius: 3px;
+        color: white;
+        font-size: 15px;
+        padding: 7px 12px;
+        border: none;
+        cursor: pointer;
+      }
+      &-close {
+        position: absolute;
+        top: 16px;
+        right: 16px;
+        transform: rotate(45deg);
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+</style>
